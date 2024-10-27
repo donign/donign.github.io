9e442c60996c200fd43eb60219cf2fc95806808a
donign <nachompmp@gmail.com>
2024-10-27 04:08:46 +0100

    Worms
    You filth shall never mess with the sacred timeline

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.getElementById('body');
+    const probability = 0.15;
+
+    console.log("Random value:", Math.random());
+
+    if (Math.random() < probability) {
+        body.style.backgroundImage = "url('images/test.gif')";
+        body.style.color = "white";
+        body.style.backgroundSize = "cover";
+        console.log("Background changed!");
+    } else {
+        console.log("Background not changed.");
+    }
+});
